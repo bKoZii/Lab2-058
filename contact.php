d007a84ff5171a0b7e0679eb4f60ae07e375fec0
Konkamon Sion <bigkonkamon@gmail.com>
2024-12-26 19:21:34 +0700

    feat: :sparkles: แก้ไขไฟล์ contact.php ครั้งที่ 2
    เพิ่ม Link หน้า รีวิวจากลูกค้า

--- contact.php
+++ contact.php
@@ -22,6 +22,7 @@
           <li><a href="room.php">ห้องพัก&ราคา</li>
           <li><a herf="guest.php">สมุดเยี่ยม</li>
           <li><a href="contact.php">ติดต่อ</li>
+          <li><a href="review.php">รีวิวจากลูกค้า</li>
         </ul>
         <p>
           <?php include "login_form.php"; ?>
